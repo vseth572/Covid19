8fe09a958b1565c3e5184bc23e45d8e8ded111bc
Vinayak <vseth572@gmail.com>
2021-06-05 23:26:22 +0530

    First Commit for Vaccination Model

--- Python_basics.py
+++ Python_basics.py
@@ -1,20 +1,24 @@
 import re
-from collections import defaultdict,Counter
+from collections import defaultdict, Counter
+import random
+
 my_regex = re.compile("[0-9]+", re.I)
 
-def double(x): return x * 2
-print(double(2))
 
+def double(x): return x * 2
+
+
+print(double(2))
 
 ### Exception Handling
 
 try:
-    print (0/0)
+    print(0 / 0)
 except ZeroDivisionError:
     print("cannot divide by zero")
 
 ### LIsts
-l1 = [1,2,3,4]
+l1 = [1, 2, 3, 4]
 print(sum(l1))
 print(l1[-1])
 print(l1[-2])
@@ -25,7 +29,7 @@ print(l1[1:-1])
 print(1 in l1)
 
 ## Extending lists
-l2 = [3,4,5]
+l2 = [3, 4, 5]
 l1.extend(l2)
 print(l1)
 l1.append("xyz")
@@ -33,28 +37,31 @@ print(l1)
 
 ### Unpacking lists
 
-x,y = [1,23]
+x, y = [1, 23]
 
 ### Tuples - immuatble cousins of the lists
 
-t = (1,2,3)
+t = (1, 2, 3)
+
+
 # t[1] = 4  #will give error
 # returning multiple values f
 
-def sum_and_product(x,y):
-    return(x+y),(x*y)
+def sum_and_product(x, y):
+    return (x + y), (x * y)
 
-print(sum_and_product(2,4))
+
+print(sum_and_product(2, 4))
 
 ## Dictionaries
 empty_dict = {}
-grades = {"Joel":80,"Tim":95}
+grades = {"Joel": 80, "Tim": 95}
 joel_grade = grades["Joel"]
 print(joel_grade)
 
-print(grades.get("Kate",0))
+print(grades.get("Kate", 0))
 
-grades["Kate"]=100
+grades["Kate"] = 100
 print(len(grades))
 
 print(grades.keys())  ## returns list of keys
@@ -68,7 +75,7 @@ print("Joel" in grades)
 
 ## Counter - it canbe primarily used to create histograms
 
-c = Counter([0,1,2,0])
+c = Counter([0, 1, 2, 0])
 print(c.most_common(1))
 
 ## Sets
@@ -82,10 +89,10 @@ s.add(2)
 
 ### Control Flow
 
-x=0
-while x<10:
-    print(x,"is less than 10")
-    x+=1
+x = 0
+while x < 10:
+    print(x, "is less than 10")
+    x += 1
 
 ##Truthiness
 
@@ -98,23 +105,75 @@ print(x is 0)
 print(all([]))
 print(any([2]))
 
-
 ## ADVANCED STUFF
 
-#sorting
-p = [4,3,2,1]
+# sorting
+p = [4, 3, 2, 1]
 b = sorted(p)
 print(y)
 
 ## List comprehensions
 
-even_numbers = [x for x in range(5) if x%2 == 0]
+even_numbers = [x for x in range(5) if x % 2 == 0]
 print(even_numbers)
-square_dict = { x:x*x for x in range(5)}
+square_dict = {x: x * x for x in range(5)}
 print(square_dict)
 
-increasing_pairs = [(x,y) for x in range(10) for y in range(x+1,10)]
+increasing_pairs = [(x, y) for x in range(10) for y in range(x + 1, 10)]
 print(increasing_pairs)
 
+
 ## Generators and Iterators
+
+def myyielder(n):
+    for i in range(n):
+        yield i
+
+
+# print(myyielder(2))
+
+##Random
+random.seed(2)
+print(random.randint(2, 5))
+print(random.randint(2, 5))
+
+## random sample
+lottery_numbers = range(60)
+winning_numbers = random.sample(lottery_numbers, 6)
+print(winning_numbers)
+
+
+## Map Reduce Filter
+
+def double(x):
+    return 2 * x
+
+
+xs = [1, 2, 3, 4]
+twice_xs = [double(x) for x in xs]
+twice_xs = list(map(double, xs))
+print(twice_xs)
+
+def is_even(x):
+    return x%2 == 0
+x_evens = filter(is_even,xs)
+print(list(x_evens))
+
+### Enumerate
+
+words = ["kite","doggo"]
+for i,x in enumerate(words):
+    print(i,x)
+
+## ZIp and Argument Unpacking
+print(list(zip([1,2,3],["abc","xyz","mno"])))
+
+## Args and Kwargs
+
+def magic(*args, **kwargs):
+    print("unnamed args",args)
+    print("kewwords args",kwargs)
+
+magic(1,key = "word")
+
 
---
+++ Vaccination_Simulation.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sklearn import linear_model
+
+#Intial data set that will be used to train the model
+initial_set = { "rate": [12,9,18,7,17,8,23,20,6,14,38,2],
+                "personnel":[12,5,20,6,20,5,25,21,5,15,18,5],
+                "purchase_power":[13,10,25,8,20,15,15,10,12,12,20,6],
+                "distance":[4,10,2,8,3,20,12,2,13,22,5,23]}
+d1 = pd.DataFrame(data = initial_set)
+d1["supplies"]=d1["personnel"]*25+d1["purchase_power"]*5
+
+#Training Data Set
+d2 = pd.DataFrame(dict(
+    personnel = np.random.randint(5,25,50),
+    purchase_power = np.random.randint(0.5,50,50),
+    distance = np.random.randint(2,30,50),))
+d2["supplies"] =d2["personnel"]*25+d2["purchase_power"]*5
+
+# d1 = d1.append(d2, ignore_index = True)
+
+X = d1[["personnel","purchase_power","distance","supplies"]]
+Y = d1[["rate"]]
+regr = linear_model.LinearRegression()
+regr.fit(X,Y)
+predicted_rate = regr.predict(d2)
+
+#Appending the predicted value of rate to the training data frame
+d2["rate"]=predicted_rate
+print(d2.corr())
+
+#Calculating the mean rate and lambda
+mean_rate = d2["rate"].mean()
+lmbda = (1/mean_rate)
+print("The value of lambda: ",lmbda)
+
