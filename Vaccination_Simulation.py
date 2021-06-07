de3512acf5a766ddfbceaa378b7d6cbedeace22b
Vinayak <vseth572@gmail.com>
2021-06-07 14:20:05 +0530

    added the exponential plots

--- Vaccination_Simulation.py
+++ Vaccination_Simulation.py
@@ -4,54 +4,60 @@ from sklearn import linear_model
 from scipy.stats import expon
 from math import exp
 import matplotlib.pyplot as plt
-#Intial data set that will be used to train the model
-initial_set = { "rate": [12,9,18,7,17,8,23,20,6,14,38,2],
-                "personnel":[12,5,20,6,20,5,25,21,5,15,18,5],
-                "purchase_power":[13,10,25,8,20,15,15,10,12,12,20,6],
-                "distance":[4,10,2,8,3,20,12,2,13,22,5,23]}
-d1 = pd.DataFrame(data = initial_set)
-d1["supplies"]=d1["personnel"]*25+d1["purchase_power"]*5
 
-#Training Data Set
+# Intial data set that will be used to train the model
+initial_set = {"rate": [12, 9, 18, 7, 17, 8, 23, 20, 6, 14, 38, 2],
+               "personnel": [12, 5, 20, 6, 20, 5, 25, 21, 5, 15, 18, 5],
+               "purchase_power": [13, 10, 25, 8, 20, 15, 15, 10, 12, 12, 20, 6],
+               "distance": [4, 10, 2, 8, 3, 20, 12, 2, 13, 22, 5, 23]}
+d1 = pd.DataFrame(data=initial_set)
+d1["supplies"] = d1["personnel"] * 25 + d1["purchase_power"] * 5
+
+# Training Data Set
 d2 = pd.DataFrame(dict(
-    personnel = np.random.randint(5,25,50),
-    purchase_power = np.random.randint(0.5,50,50),
-    distance = np.random.randint(2,30,50),))
-d2["supplies"] =d2["personnel"]*25+d2["purchase_power"]*5
+    personnel=np.random.randint(5, 25, 50),
+    purchase_power=np.random.randint(0.5, 50, 50),
+    distance=np.random.randint(2, 30, 50), ))
+d2["supplies"] = d2["personnel"] * 25 + d2["purchase_power"] * 5
 
 # d1 = d1.append(d2, ignore_index = True)
 
-X = d1[["personnel","purchase_power","distance","supplies"]]
+X = d1[["personnel", "purchase_power", "distance", "supplies"]]
 Y = d1[["rate"]]
 regr = linear_model.LinearRegression()
-regr.fit(X,Y)
+regr.fit(X, Y)
 predicted_rate = regr.predict(d2)
 
-#Appending the predicted value of rate to the training data frame
-d2["rate"]=predicted_rate
+# Appending the predicted value of rate to the training data frame
+d2["rate"] = predicted_rate
 print(d2.corr())
 
-#Calculating the mean rate and lambda
+# Calculating the mean rate and lambda
 mean_rate = d2["rate"].mean()
-lmbda = (1/mean_rate)
-print("The value of lambda: ",lmbda)
+lmbda = (1 / mean_rate)
+print("The value of lambda: ", lmbda)
+
+""" Function that takes population in thosands as an argument and populates
+a dataframe of day and cumulative population vaccinated"""
 
 
-
-## Function to
 def populate(p):
-    x = pd.DataFrame(columns =["day","cumulative_population_covered"])
+    x = pd.DataFrame(columns=["day", "cumulative_population_covered"])
     i = 1
-    while((1-exp(-i*lmbda))<=0.999):
-        x.loc[i-1] = [i,p*(1-exp(-i*lmbda))]
-        i+=1
+    while (1 - exp(-i * lmbda)) <= 0.999:
+        x.loc[i - 1] = [i, p * (1 - exp(-i * lmbda))]
+        i += 1
     return x
 
+"""The Population for a city/region can be passed as a list or 
+it can also be loaded from a csv file"""
 Delhi = populate(2000)
 Mumbai = populate(1500)
 
-plt.plot(Delhi["day"], Delhi["cumulative_population_covered"],'r')
-plt.plot(Mumbai["day"], Mumbai["cumulative_population_covered"],'b')
+"""Plotting the Exponential Curve for Delhi and Mumbai"""
+plt.plot(Delhi["day"], Delhi["cumulative_population_covered"], 'r',label="Delhi")
+plt.xlabel("Days")
+plt.ylabel("Population Vaccinated")
+plt.plot(Mumbai["day"], Mumbai["cumulative_population_covered"], 'b',label="Mumbai")
+plt.legend(loc=2)
 plt.show()
-
-
